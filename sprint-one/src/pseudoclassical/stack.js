275d4dc4429cf1c7fd50cbce8f9875bdf7b9447b
strangeInference <tesla.phipps@gmail.com>
2015-11-18 17:24:24 -0800

    Completed push() for pseudoclassical stack

--- sprint-one/src/pseudoclassical/stack.js
+++ sprint-one/src/pseudoclassical/stack.js
@@ -2,8 +2,13 @@ var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
   this.stackSize = 0;
-  this.starage = {};
+  this.storage = {};
 };
+
+Stack.prototype.push = function(val) {
+  this.storage[this.stackSize] = val;
+  this.stackSize++;
+}
 
 Stack.prototype.size = function(){
   return this.stackSize >= 0 ? this.stackSize : 0;
