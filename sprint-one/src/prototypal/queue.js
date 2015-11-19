ff0ab255bf5849b845a0f91360ec6c70b88b9083
strangeInference <tesla.phipps@gmail.com>
2015-11-18 16:41:05 -0800

    Completed dequeue() for prototypal gueue.

--- sprint-one/src/prototypal/queue.js
+++ sprint-one/src/prototypal/queue.js
@@ -12,6 +12,17 @@ var queueMethods = {
     this.queueSize++;
   },
 
+  dequeue: function(){
+    var temp = this[0];
+    delete this[0];
+    for(var i = 1; i < this.queueSize; i++){
+      this[i-1] = this[i];
+      delete this[i];
+    }
+    this.queueSize--;
+    return temp;
+  },
+
   size: function(){
     return this.queueSize >= 0 ? this.queueSize : 0;
   }
