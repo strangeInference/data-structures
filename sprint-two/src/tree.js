3f9c48d3b13f3f82be239a04bae7a76ec8e9cafc
strangeInference <tesla.phipps@gmail.com>
2015-11-19 10:47:07 -0800

    Delegated Tree lookups to treeMethods object

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -1,5 +1,6 @@
 var Tree = function(value) {
-  var newTree = {};
+  // delegate failed lookups to treeMethods object
+  var newTree = Object.create(treeMethods);
   newTree.value = value;
 
   // your code here
