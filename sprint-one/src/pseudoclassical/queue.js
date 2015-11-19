df8d4e13229440ec0e3c738c2dc673c6fe749d79
strangeInference <tesla.phipps@gmail.com>
2015-11-18 17:27:41 -0800

    Completed size() and enqueue() for pseudoclassical queue

--- sprint-one/src/pseudoclassical/queue.js
+++ sprint-one/src/pseudoclassical/queue.js
@@ -1,6 +1,17 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  this.queueSize = 0;
+  this.storage = {};
+};
+
+Queue.prototype.enqueue = function(val) {
+  this.storage[this.queueSize] = val;
+  this.queueSize++;
+}
+
+Queue.prototype.size = function() {
+  return this.queueSize >= 0 ? this.queueSize : 0;
 };
 
 
