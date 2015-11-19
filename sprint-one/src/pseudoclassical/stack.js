2095f7441a5d94dec32cf2c89dfbc9d157076c53
strangeInference <tesla.phipps@gmail.com>
2015-11-18 17:22:32 -0800

    Completed size() for psudoclassical stack.

--- sprint-one/src/pseudoclassical/stack.js
+++ sprint-one/src/pseudoclassical/stack.js
@@ -1,6 +1,12 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  this.stackSize = 0;
+  this.starage = {};
 };
+
+Stack.prototype.size = function(){
+  return this.stackSize >= 0 ? this.stackSize : 0;
+}
 
 
