965e155632a1f42dd971dfae288593ac4a519c9e
strangeInference <tesla.phipps@gmail.com>
2015-11-19 10:27:13 -0800

    Completed addToTail for LinkedList

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -2,11 +2,25 @@ var LinkedList = function() {
   var list = {};
   list.head = null;
   list.tail = null;
+  // list.length = 
 
   list.addToTail = function(value) {
+    var node = Node(value);
+    
+    // if LinkedList is empty
+    if (this.head === null) {
+      this.head = node;
+      this.tail = node;
+    }
+    // update LinkedList with new node
+    else {
+      list.tail.next = node;
+      list.tail = node;
+    }
   };
 
   list.removeHead = function() {
+
   };
 
   list.contains = function(target) {
