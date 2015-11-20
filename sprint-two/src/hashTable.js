1af524d41950d3d5cdac338e9262651601f87a18
strangeInference <tesla.phipps@gmail.com>
2015-11-19 20:03:00 -0800

    Rewrote insert, remove, and retrieve to avoid hash function collisions. Answered time complexity questions. Passes all non-pending specs.

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -7,16 +7,27 @@ var HashTable = function() {
 
 HashTable.prototype.insert = function(k, v) {
   var index = getIndexBelowMaxForKey(k, this._limit);
-  this._storage.set(index, v);
+  
+  // if hashTable is undefined at index, then add a new object
+  if (this._storage.get(index) === undefined) {
+    var temp = {};
+    temp[k] = v;
+    this._storage.set(index, temp);
+  }
+  // if hashTable is defined at index, then augment existing object
+  else {
+    this._storage.get(index)[k] = v;
+  }
 };
 
 HashTable.prototype.retrieve = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
-  return this._storage.get(index);
+  return this._storage.get(index)[k];
 };
 
 HashTable.prototype.remove = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  delete this._storage.get(index)[k];
 };
 
 
@@ -24,5 +35,9 @@ HashTable.prototype.remove = function(k) {
 /*
  * Complexity: What is the time complexity of the above functions?
  */
-
+/*
+  insert(): O(1), but hash function is O(n)
+  retrieve(): O(1), but hash function is O(n)
+  remove(): O(1), but hash function is O(n)
+*/
 
