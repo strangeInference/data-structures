6930d869183e1d6fbe7c4c30e058cbdff961eb11
strangeInference <tesla.phipps@gmail.com>
2015-11-19 16:10:06 -0800

    Completed hasEdge(), addEdge(), removeEdge() for Graph

--- sprint-two/src/graph.js
+++ sprint-two/src/graph.js
@@ -41,16 +41,86 @@ Graph.prototype.removeNode = function(node) {
 // ------------------------
 // Returns a boolean indicating whether two specified nodes are connected.  Pass in the values contained in each of the two nodes.
 Graph.prototype.hasEdge = function(fromNode, toNode) {
+  var found = false;
+  _.each(this.nodeList, function(node) {
+      if (node.value === fromNode) {
+        _.each(node.connections, function(edge) {
+          if (edge.value === toNode) {
+            found = true;
+          }
+        });
+      }
+    });
+  return found;
 };
+
 
 // ------------------------
 // Connects two nodes in a graph by adding an edge between them.
 Graph.prototype.addEdge = function(fromNode, toNode) {
+  var fromNodeIndex;
+  var toNodeIndex;
+
+  // find indices of fromNode and toNode
+  _.each(this.nodeList, function(node, i) {
+    if (node.value === fromNode) {
+      fromNodeIndex = i;
+    }
+    if (node.value === toNode) {
+      toNodeIndex = i;
+    }
+  });
+  this.nodeList[fromNodeIndex].connections.push(this.nodeList[toNodeIndex]);
+  this.nodeList[toNodeIndex].connections.push(this.nodeList[fromNodeIndex]);
 };
 
 // ------------------------
 // Remove an edge between any two specified (by value) nodes.
 Graph.prototype.removeEdge = function(fromNode, toNode) {
+  var fromNodeIndex;
+  var toNodeIndex;
+  // find indices of fromNode and toNode
+  _.each(this.nodeList, function(node, i) {
+    if (node.value === fromNode) {
+      fromNodeIndex = i;
+    }
+    if (node.value === toNode) {
+      toNodeIndex = i;
+    }
+  });
+
+  var fromConnections = this.nodeList[fromNodeIndex].connections;
+  var toConnections = this.nodeList[toNodeIndex].connections;
+
+  var fromConnectionsIndex;
+  var toConnectionsIndex;
+
+  _.each(fromConnections, function(node, i) {
+    if (node.value === toNode) {
+      //fromConnections = fromConnections.slice(0, i) + fromConnections.slice(i + 1);  
+      fromConnectionsIndex = i;
+    }
+  });
+
+  this.nodeList[fromNodeIndex].connections = fromConnections.slice(0, fromConnectionsIndex) + fromConnections.slice(fromConnectionsIndex + 1);
+  
+  _.each(toConnections, function(node, i) {
+    if (node.value === fromNode) {
+      //toConnections = toConnections.slice(0, i) + toConnections.slice(i + 1);  
+      toConnectionsIndex = i;
+    }
+  });
+
+  this.nodeList[toNodeIndex].connections = toConnections.slice(0, toConnectionsIndex) + toConnections.slice(toConnectionsIndex + 1);
+
+  // find the fromNode
+  // look through fromNode.connections to find toNode
+  // remove toNode from fromNode.connections
+  // look through toNode.connections to find fromNode
+  // remove fromNode from toNode.connections
+
+  // look through nodeList for both nodes
+
 };
 
 // ------------------------
