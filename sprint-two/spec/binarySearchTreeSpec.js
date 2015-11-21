2bd9e0fba51e7d8164edc814bf5bbb33c2407932
strangeInference <tesla.phipps@gmail.com>
2015-11-21 11:00:59 -0800

    Edited test suite for breadthFirstLog() for BinarySearchTree

--- sprint-two/spec/binarySearchTreeSpec.js
+++ sprint-two/spec/binarySearchTreeSpec.js
@@ -37,16 +37,16 @@ describe('binarySearchTree', function() {
     expect(array).to.eql([5,2,3]);
   });
 
-  it('should execute a callback on every value in a tree using "depthFirstLog"', function(){
+  it('should execute a callback on every value in a tree using "breadthFirstLog"', function(){
     var array = [];
     var func = function(value){ array.push(value); };
     binarySearchTree.insert(3);
     binarySearchTree.insert(7);
     binarySearchTree.insert(1);
-    binarySearchTree.insert(2);
+    binarySearchTree.insert(4);
     binarySearchTree.insert(6);
     binarySearchTree.insert(8);
     binarySearchTree.breadthFirstLog(func);
-    expect(array).to.eql([5,3,7,1,2,6,8]);
+    expect(array).to.eql([5,3,7,1,4,6,8]);
   });
 });
